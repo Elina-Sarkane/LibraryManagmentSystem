fc4f02f09380fa2b3bca5b86167977135cf7f576
Elīna Sarkane <75952160+Sarkanaaaa@users.noreply.github.com>
2021-07-30 12:47:43 +0300

    Delete src/main/resources directory

--- src/main/resources/booklist.sql
+++
@@ -1,8 +0,0 @@
-select * from books;
-
-insert into books(author, title, year, pages, amount)
-values 
-('Veronica Roth', 'Divergient', '2011', '487', '6'),
-('Lauren Kate', 'Fallen', '2009', '452', '7'),
-('Alan A. Milne', 'Winnie the Pooh', '1926', '176', '3'),
-('Roald Dahl', 'Charlie and the Chocolate Factory', '1979', '192', '2');
